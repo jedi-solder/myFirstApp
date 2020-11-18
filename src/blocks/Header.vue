2490773bef87635681c17848faa40ff5a18b0138
Vladimir <bassoldatov@gmail.com>
2020-11-18 18:19:01 +0500

    header is ready

---
+++ src/blocks/Header.vue
@@ -0,0 +1,42 @@
+<template>
+  <header>
+    <div class="container headerContainer">
+      <Logo></Logo>
+      <Headercontacts></Headercontacts>
+      <Search></Search>
+    </div>
+  </header>
+</template>
+
+<script>
+// eslint-disable-next-line import/extensions
+import Logo from '@/components/Logo';
+// eslint-disable-next-line import/extensions
+import Headercontacts from '@/components/Headercontacts';
+// eslint-disable-next-line import/extensions
+import Search from '@/components/Search';
+
+export default {
+  name: 'Header',
+  components: {
+    Search,
+    Headercontacts,
+    Logo,
+  },
+};
+</script>
+
+<style scoped>
+header {
+  width: 100vw;
+  height: 85px;
+  background-color: #000000;
+  opacity: 0.5;
+}
+.headerContainer {
+  display: flex;
+  height: inherit;
+  justify-content: space-around;
+  align-items: center;
+}
+</style>
---
+++ src/components/Headercontacts.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="headerContacts">
+    <div class="headerContacts__phone">
+      <img src="../assets/icon/phone.svg" alt="phone">
+      <span>+123 456 7890</span>
+    </div>
+    <div class="headerContacts__emailAddress">
+      <img src="../assets/icon/email.svg" alt="email">
+      <span>info@example.com</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Headercontacts',
+};
+</script>
+
+<style scoped>
+.headerContacts {
+  width: 400px;
+  color: #ffffff;
+  font-size: 17px;
+  font-weight: 400;
+  letter-spacing: 0.36px;
+  display: flex;
+  justify-content: space-between;
+}
+img {
+  margin-right: 14px;
+  height: 25px;
+}
+.headerContacts__phone,
+.headerContacts__emailAddress {
+  display: flex;
+}
+</style>
