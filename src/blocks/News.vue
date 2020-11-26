16ba761718c261d16623abd08b66c9171a6e8ed2
jedi-solder <soldatovvn91@gmail.com>
2020-11-26 16:17:59 +0500

    some

---
+++ src/blocks/News.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="newsAndEvents">
+    <div class="container">
+      <Headermt title="News & Events"></Headermt>
+      <div class="newsAndEvents__wrap">
+        <div class="news">
+          <h2>Latest News</h2>
+        </div>
+        <div class="events"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// eslint-disable-next-line import/extensions
+import Headermt from '@/components/Headermt';
+
+export default {
+  name: 'News',
+  components: { Headermt },
+};
+</script>
+
+<style scoped>
+
+</style>
